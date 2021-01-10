46f2da7c394d1c7958f357d51318cafa22bcbcc4
NguyenTanPil <traila1008@gmail.com>
2021-01-10 12:39:19 +0700

    Edit index.py

--- index.py
+++ index.py
@@ -1,12 +1,7 @@
-import nltk, re, pprint
-from nltk import CFG, data
+import nltk
+from nltk import data
 from nltk.tree import Tree
 from nltk.draw.tree import draw_trees
-# Load a grammar from to file
-grammar = nltk.data.load('file:grammar.cfg')
-# grammar = nltk.CFG.fromstring(grammarInput)
-# print grammar
-print(grammar)
 # Check valid of input string
 def checkString(sent):
 	try:
@@ -17,13 +12,13 @@ def checkString(sent):
 				return False
 		# check string in set terminal
 		for i in parser.parse(sent):
-			pass
-		return True
+			return True
 	except:
 		print("String must be terminals or non-terminals")
 		return False
-
-# main
+# Load a grammar from to file
+grammar = nltk.data.load('file:grammar.cfg')
+print(grammar)
 inputString = input('Input string: ')
 parser = nltk.ChartParser(grammar)
 sent = list(map(str, inputString))
@@ -31,17 +26,14 @@ if checkString(sent):
 	lst = []
 	for i in parser.parse(sent):
 		lst.append(i)
-
-# main	
+# output
 	if len(lst) > 0:
 		print('String ' + inputString + ' was born from grammar')
 		if len(lst) > 1: 
 			print('The grammar is ambiguity grammar')
-			# print(lst[0])
 			draw_trees(lst[0], lst[1])
 		else:
 			print('The grammar is not ambiguity grammar')
-			# print(lst[0])
 			draw_trees(lst[0])
 	else:
 		print('String ' + inputString + ' was\'t born from grammar')
